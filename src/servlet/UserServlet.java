d88832aee81a7056390648a0b1873f1d4188a721
YYece <hellenjyy@gmail.com>
2018-03-12 21:45:25 +0800

    first

---
+++ src/servlet/UserServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserServlet" ,urlPatterns="/LoginServlet")
+public class UserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       String username =  request.getParameter("username");
+       String password =  request.getParameter("password");
+        System.out.println("username"+username);
+        System.out.println("password"+password);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+}
